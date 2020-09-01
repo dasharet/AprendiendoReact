1472c6b7b422d211c5434b2c94b5fbefd038451f
Joselyn <dasharethdelvalle@hotmail.com>
2020-08-31 21:14:55 -0600

    hola mundo react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './assets/images/logo.svg';
+import './assets/css/App.css';
+
+//Importar componentes
+import MiComponente from './components/MiComponente';
+
+function HolaMundo(nombre,edad){
+  var presentacion = (
+  <div>
+    <h2>Hola, soy {nombre}</h2>
+    <h3>Tengo {edad} años</h3>
+  </div>
+  );
+
+  return presentacion;
+}
 
 function App() {
-  return (
+  var nombre = "Joselyn";
+
+    return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {
+          HolaMundo(nombre,25)
+        }   
+      <section className="componentes">
+        <MiComponente />
+        <MiComponente />
+        <MiComponente />
+        <MiComponente />
+      </section>    
       </header>
     </div>
   );
